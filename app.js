02b709cce3696f55dc992e239bbfe687d12273e3
Sara Holm <sara.holm@live.com>
2020-09-14 13:48:05 +0200

    passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const FileStore = require("session-file-store")(session);
+const passport = require("passport");
+const authenticate = require("./authenticate");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -42,23 +44,19 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-const auth = (req, res, next) => {
-  console.log(req.session);
 
-  if (!req.session.user) {
+const auth = (req, res, next) => {
+  if (!req.user) {
     const err = new Error("You are not authenticated!");
     err.status = 403;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      const err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 };
 
